7bb4998372f6cdcf01e0724efc17ed965e9de17f
sh-miyoshi <s.miyoshi@jp.fujitsu.com>
2018-11-28 05:21:09 +0000

    fix bug

--- system/gitlab-runner-cleanup.sh
+++ system/gitlab-runner-cleanup.sh
@@ -5,13 +5,13 @@ tmp=$(mktemp)
 names=("projfrontend" "projbackend" "staging" "production")
 
 for name in ${names[@]}; do
+  kubectl delete secret gitlab -n $name
   sed -e "s/<namespace>/$name/g" template/gitlab-user.yaml > $tmp
   kubectl delete -f $tmp
-  kubectl delete secret gitlab -n $name
 done
 
 helm delete --purge projfrontend-runner
 helm delete --purge projbackend-runner
-helm delete --purge rojops-runner
+helm delete --purge projops-runner
 
 rm -rf $tmp
